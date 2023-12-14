791adb316311124511de7cf0d11bb494c1dcfada
Collins <oriakhicolls01@gmail.com>
2023-12-15 00:19:51 +0100

    Resolved fonts

--- lib/utils/text_styles.dart
+++ lib/utils/text_styles.dart
@@ -6,54 +6,64 @@ TextStyle headingStyle1 = const TextStyle(
   fontFamily: 'Satoshi',
   fontWeight: FontWeight.w600,
 );
-TextStyle headingStyle2 = GoogleFonts.poppins(
+TextStyle headingStyle2 = const TextStyle(
   height: 1.5,
   fontSize: 30,
-  fontWeight: FontWeight.w700,
+  fontFamily: 'Satoshi',
+  fontWeight: FontWeight.w600,
 );
-TextStyle headingStyle3 = GoogleFonts.dmSans(
+TextStyle headingStyle3 = const TextStyle(
   height: 1.5,
   fontSize: 24,
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Satoshi',
 );
-TextStyle headingStyle4 = GoogleFonts.poppins(
+TextStyle headingStyle4 = const TextStyle(
   height: 1.5,
   fontSize: 22,
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w600,
+  fontFamily: 'Satoshi',
 );
-TextStyle headingStyle5 = GoogleFonts.poppins(
+TextStyle headingStyle5 = const TextStyle(
   height: 1.5,
   fontSize: 18,
   fontWeight: FontWeight.w500,
+  fontFamily: 'Satoshi',
 );
-TextStyle headingStyle6 = GoogleFonts.poppins(
+TextStyle headingStyle6 = const TextStyle(
   height: 1.5,
+  fontFamily: 'Satoshi',
   fontSize: 18,
   fontWeight: FontWeight.w500,
 );
-TextStyle bodyStyle1 = GoogleFonts.poppins(
+TextStyle bodyStyle1 = const TextStyle(
   height: 1.5,
   fontSize: 14,
   fontWeight: FontWeight.w400,
+  fontFamily: 'Satoshi',
 );
 
-TextStyle bodyStyle2 = GoogleFonts.poppins(
+TextStyle bodyStyle2 = const TextStyle(
   height: 1.5,
   fontSize: 12,
   fontWeight: FontWeight.w500,
+  fontFamily: 'Satoshi',
 );
 
-TextStyle captionStyle = GoogleFonts.poppins(
+TextStyle captionStyle = const TextStyle(
   height: 1.5,
   fontSize: 10,
   fontWeight: FontWeight.w400,
+  fontFamily: 'Satoshi',
 );
-TextStyle buttonStyle = GoogleFonts.poppins(
+TextStyle buttonStyle = const TextStyle(
   height: 1.5,
   fontSize: 16,
+  fontFamily: 'Satoshi',
   fontWeight: FontWeight.w600,
 );
-TextStyle small = GoogleFonts.poppins(
+TextStyle small = const TextStyle(
   fontSize: 8,
   fontWeight: FontWeight.w400,
+  fontFamily: 'Satoshi',
 );
